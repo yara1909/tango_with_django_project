39d5f3cdceebb5ad292d9466c6e13e0efeca213d
Yara Bakour <yarabakour@Yaras-MacBook-Pro.local>
2021-02-09 19:06:27 +0400

    Chapter 6 done

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -5,7 +5,7 @@ class PageAdmin(admin.ModelAdmin):
      list_display = ('title', 'category', 'url')
      
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
+    prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
