aeab5c9a691c493c6312884e2f7640e554bce556
Yara Bakour <yarabakour@Yaras-MacBook-Pro.local>
2021-02-10 23:35:20 +0400

    Chapter 8

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-      name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+      name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
       views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
       likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
       slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -12,8 +12,8 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
         
 class PageForm(forms.ModelForm):
-     title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-     url = forms.URLField(max_length=200, help_text="Please eneter the URL of the page.")
+     title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
+     url = forms.URLField(max_length=Page.URL_MAX_LENGTH, help_text="Please eneter the URL of the page.")
      views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
      
      class Meta:
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -43,7 +43,7 @@ def add_category(request):
        
        if form.is_valid():
           form.save(commit=True)
-          return redirect('/rango/')
+          return redirect(reverse('rango:index'))
        else:
           print(form.errors)
    return render(request, 'rango/add_category.html', {'form': form})
@@ -55,7 +55,7 @@ def add_page(request, category_name_slug):
       category = None
    
    if category is None:
-      return redirect('/rango/')
+      return redirect(reverse('rango:index'))
       
    form = PageForm()
    
